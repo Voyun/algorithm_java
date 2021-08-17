ff5ce1ba5153d57a16212ed41c0fe1bc20b896af
Voyun <voyun0819@gmail.com>
2021-08-17 18:31:34 +0900

    SBT_HM&TS 3.매출액의 종류

---
+++ SBT_Array/src/hashmap_treeset/TypeOfSales.java
@@ -0,0 +1,43 @@
+package hashmap_treeset;
+import java.util.*;
+
+public class TypeOfSales {
+	
+	public ArrayList<Integer> solution(int n, int k, int[] arr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<k-1; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+		}
+		
+		int lt=0;
+		for(int rt=k-1; rt<n; rt++) {
+			map.put(arr[rt], map.getOrDefault(arr[rt], 0)+1);
+			answer.add(map.size());
+			map.put(arr[lt], map.get(arr[lt])-1);
+			if(map.get(arr[lt])==0) map.remove(arr[lt]);
+			lt++;
+		}
+		
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		TypeOfSales T = new TypeOfSales();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int m = kb.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		
+		for(int x: T.solution(n, m, arr)) {
+			System.out.print(x + " ");
+		}
+
+	}
+
+}
